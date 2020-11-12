d7963ed84981ee520bea98bb1efd15aa95859026
ajaysirsat <ajaysirsat44@gmail.com>
2020-11-12 16:07:10 +0400

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,20 @@
+#This is my first python program
+
+print("This program will do all arithmetic operations")
+no1 = int(input('Enter first number:'))
+no2 = int(input('Enter Second number:'))
+
+add = no1 + no2;
+sub = no1 - no2;
+mul = no1 * no2;
+div = no1 / no2;
+mod = no1 % no2;
+print("---------------------------------------------")
+print("Addition of ",no1,"&",no2,"is:",add)
+print("Substraction of ",no1,"&",no2,"is:",sub)
+print("Multiplication of ",no1,"&",no2,"is:",mul)
+print("Division of ",no1,"&",no2,"is:",div)
+print("Mod of ",no1,"&",no2,"is:",mod)
+print("---------------------------------------------")
+
+print("End of the program.....")
