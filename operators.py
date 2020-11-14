b7010d7d234ba5e4bf11f5f2e6e4f282a45d0c3f
ajaysirsat <ajaysirsat44@gmail.com>
2020-11-14 12:33:25 +0400

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,32 @@
+# Operators in Python
+
+# Addition +
+# Substraction -
+# Multiplication *
+# Division /
+# Floor Division //
+# Exponent **
+# Reminder %
+
+a = 5
+b = 2
+result = a + b
+print("Add:",result)
+
+result = a - b
+print("Sub:",result)
+
+result = a * b
+print("Mul:",result)
+
+result = a ** b
+print("Exponent:",result)
+
+result = a / b
+print("Div:",result)
+
+result = a // b
+print("Quotient:",result)
+
+result = a % b
+print("Remaindar:",result)
